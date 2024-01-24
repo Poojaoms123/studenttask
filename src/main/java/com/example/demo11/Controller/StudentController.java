1862b4a59b661a00e16644f4eea51ff6d4ed12ac
Pooja Suryawanshi <pooja.omsoftware1@gmail.com>
2024-01-24 11:37:47 +0530

    apichange

--- src/main/java/com/example/demo11/Controller/StudentController.java
+++ src/main/java/com/example/demo11/Controller/StudentController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api1")
 public class StudentController {
     @Autowired
     StudentService studentService;
